cc7a9c997c80f5e97afda07d141a0ead0140e896
kashish0622 <kashishbhattofficial@gmail.com>
2026-03-20 13:47:37 +0530

    different cases on variable names

---
+++ src/variables/cases.py
@@ -0,0 +1,14 @@
+#camel_case
+#each word except the first one starts with capital letter
+myFullName = "Kashish Bhatt"
+print(myFullName)
+
+#pascal_case
+#each word starts with capital letter
+MyFullName = "Kashish Bhatt"
+print(MyFullName)
+
+#snake_case
+#each word is separated by an underscore character
+my_full_name = "Kashish Bhatt"
+print("Kashish Bhatt")
