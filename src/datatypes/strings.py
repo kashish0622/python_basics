56982cc7f6956b5c68177263cf5b4461e5474c05
kashish0622 <kashishbhattofficial@gmail.com>
2026-03-20 14:36:46 +0530

    declared string, did string indexing, len function and string slicing

---
+++ src/datatypes/strings.py
@@ -0,0 +1,12 @@
+a = '''This is my python string.
+And i will practice different operations and functions that can be applied in string.'''
+print(a)
+
+#string indexing
+print(a[12])
+
+#length of string
+print(len(a))
+
+#string slicing
+print(a[0 : 5 : 1])
