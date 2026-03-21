67a5938c82e414f9b6e6d956647ca3f5a543fd36
kashish0622 <kashishbhattofficial@gmail.com>
2026-03-21 18:25:32 +0530

    number guess game created!!

---
+++ practicals/numberguess.py
@@ -0,0 +1,14 @@
+print("Number guessing game")
+user = str(input("Enter your name: "))
+print("Hello", user)
+num = 96
+guess = int(input("Enter number: "))
+while guess != num:
+    if guess > num:
+       print('''You entered a higher number.
+    Try again!!''')
+    else:
+       print('''You entered a lower number.
+          Try again!!''')
+    guess = int(input("Enter the number again: "))
+    print("Congratulation!Your guess is correct this time.")
